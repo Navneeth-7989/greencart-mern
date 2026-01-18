f471ac80dd04bcf5ac161eb0af16c2f2ba86bbf4
Navneeth-7989 <navneeth9966@gmail.com>
2026-01-18 14:39:14 +0530

    feat: compelte footer sectoin

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import {Toaster} from "react-hot-toast"
+import { Toaster } from "react-hot-toast"
 import Navbar from './components/Navbar'
 import { Routes, Route, useLocation } from 'react-router-dom'
 import Home from './pages/Home'
+import Footer from './components/Footer'
 
 
 const App = () => {
@@ -10,15 +11,16 @@ const App = () => {
   const isSellerPath = useLocation().pathname.includes("seller");
   return (
     <div>
-     {isSellerPath ? null :  <Navbar />}
+      {isSellerPath ? null : <Navbar />}
 
-<Toaster/>
+      <Toaster />
 
       <div className={`${isSellerPath ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"}`}>
         <Routes>
-          <Route path='/' element={<Home/>}/>
+          <Route path='/' element={<Home />} />
         </Routes>
       </div>
+     {!isSellerPath && <Footer/>} 
     </div>
 
   )
