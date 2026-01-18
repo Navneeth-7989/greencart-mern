7c0411242e6567b7626d9fb7edfddc023ae7224d
Navneeth-7989 <navneeth9966@gmail.com>
2026-01-18 13:31:06 +0530

    feat: complete bestSeller Section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import {Toaster} from "react-hot-toast"
 import Navbar from './components/Navbar'
 import { Routes, Route, useLocation } from 'react-router-dom'
 import Home from './pages/Home'
+
 
 const App = () => {
 
@@ -9,6 +11,9 @@ const App = () => {
   return (
     <div>
      {isSellerPath ? null :  <Navbar />}
+
+<Toaster/>
+
       <div className={`${isSellerPath ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"}`}>
         <Routes>
           <Route path='/' element={<Home/>}/>
