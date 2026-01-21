dec33b86e9e9edb8cdad87d8637d40c573d5730e
Navneeth-7989 <navneeth9966@gmail.com>
2026-01-21 05:45:32 +0530

    feat: complete all required routes

--- server/server.js
+++ server/server.js
@@ -5,12 +5,19 @@ import connectDB from './configs/db.js';
 import 'dotenv/config'
 import userRouter from './routes/userRoute.js';
 import sellerRouter from './routes/sellerRoute.js';
+import connectCloudinary from './configs/cloudinary.js';
+import productRouter from './routes/productRoute.js';
+import cartRouter from './routes/cartRoute.js';
+import addressRouter from './routes/addressRoute.js';
+import orderRouter from './routes/orderRoute.js';
 
 const app = express()
 
 const port = process.env.PORT || 4000;
 
 await connectDB()
+
+await connectCloudinary()
 
 
 
@@ -28,7 +35,10 @@ app.get('/', (req, res) =>
 
 app.use('/api/user', userRouter)
 app.use('/api/seller', sellerRouter)
-
+app.use('/api/product', productRouter)
+app.use('/api/cart', cartRouter)
+app.use('/api/address', addressRouter)
+app.use('/api/order', orderRouter)
 
 app.listen(port, ()=>{
     console.log(`Server is running on http://localhost:${port}`)
